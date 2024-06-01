7528fce074c41bb4adc2d5ce286d262fbf2722ad
Dishant Sharma <104745005+sharmadishant@users.noreply.github.com>
2024-06-01 14:49:36 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.header("Hi there, Welcome to FLats and House Recommendation system 👋")
+
+st.subheader("-------------------------------------------------------------------------------")
+
+st.subheader("We are providing you a website that can help you to buy your dream house in your budget.")
+
+st.write("-----------------------------------------------------------------------------------------")
+
+st.write('''You can predict the price of your desired flats and house that you want. Also, you have chance to analyze
+        and visualize the price and distribution that will help you to understand the compraing the price of flats and 
+        house b/w different type of areas and sectors''')
+
+st.write('''If want similar type of apartments that have similar types of facitlities than our Recommender system will help you
+        to find flats and houses that have similar type features''')
+
+
+st.sidebar.success("Select a demo above.")
+
+
+
+
+
+
